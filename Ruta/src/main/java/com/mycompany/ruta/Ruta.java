a68f3d90ebfdeb43e758dae2ecf02c4347289af5
feliUSa <daniel.oviedo01@usa.edu.co>
2024-03-11 20:21:28 -0500

    Update sockets

--- Ruta/src/main/java/com/mycompany/ruta/Ruta.java
+++ Ruta/src/main/java/com/mycompany/ruta/Ruta.java
@@ -2,24 +2,32 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.ruta;
+ package com.mycompany.ruta;
 
-import javax.swing.SwingUtilities;
-
-import com.mycompany.ruta.controller.controller;
-import com.mycompany.ruta.view.View;
-
-/**
- *
- * @author juan
- */
-public class Ruta {
-
-    public static void main(String[] args) {
-
-        SwingUtilities.invokeLater(() -> {
-            View vista = new View();
-            controller controlador = new controller(vista);
-        });
-    }
-}
+ import java.lang.ModuleLayer.Controller;
+ 
+ import javax.swing.SwingUtilities;
+ 
+ import com.mycompany.ruta.controller.controller;
+ 
+ import com.mycompany.ruta.view.View;
+ 
+ /**
+  *
+  * @author juan
+  */
+ public class Ruta {
+ 
+     public static void main(String[] args) {
+ 
+         //SwingUtilities.invokeLater(() -> {
+             View vista = new View();
+             controller controlador = new controller(vista, 4); // Establece el máximo de jugadores
+             if (args.length == 0) {
+                 controlador.iniciarServidor();
+             } else {
+                 controlador.connectToServer(args[0], Integer.parseInt(args[1]));
+             }
+         //});
+     }
+ }
